b287caa7cccebc9e19db1bcb2de5a82e2b6239c0
Madhusudan Bhukta <166828632+megabytis@users.noreply.github.com>
2025-11-09 15:28:34 +0530

    Add signup data validation logic

---
+++ server/src/utils/validate.js
@@ -0,0 +1,34 @@
+const validator = require("validator");
+
+const validateSignupData = (req) => {
+  const { name, email, password, role, cart } = req.body;
+
+  if (!name) {
+    throw new Error("Name not valid!");
+  } else if (!validator.isEmail(email)) {
+    throw new Error("Email not valid!");
+  } else if (!validator.isStrongPassword(password)) {
+    throw new Error("Password is not strong!");
+  } else if (!role) {
+    throw new Error("Role has not provided!");
+  } else if (
+    !(
+      validator.matches(role.toString(), "user") ||
+      validator.matches(role.toString(), "admin")
+    )
+  ) {
+    throw new Error("Not a valid role type!");
+  } else if (cart) {
+    if (cart[0].length > 0) {
+      if (!validator.isMongoId(cart[0].productId)) {
+        throw new Error("Invalid productID");
+      } else if (cart[0].quantity < 1) {
+        throw new Error("Insufficient order Quantity!");
+      }
+    }
+  }
+};
+
+module.exports = {
+  validateSignupData,
+};
