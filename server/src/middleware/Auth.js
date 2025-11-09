60eebfab7394eaa0151e8db84bc1c3c8057c2b54
Madhusudan Bhukta <166828632+megabytis@users.noreply.github.com>
2025-11-09 21:47:02 +0530

    Implemented user authentication middleware

---
+++ server/src/middleware/Auth.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken");
+const { userModel } = require("../models/user");
+
+const userAuth = async (req, res, next) => {
+  try {
+    const { token } = req.cookies;
+
+    if (!token) {
+      throw new Error("Invalid Token!");
+    }
+
+    const foundUserObj = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+    const foundUser = await userModel.findById(foundUserObj._id);
+
+    if (!foundUser) {
+      throw new Error("User not found!");
+    }
+
+    req.user = foundUser;
+
+    next();
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = {
+  userAuth,
+};
