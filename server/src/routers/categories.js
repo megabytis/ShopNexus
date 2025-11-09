0b4e6a2c3d6bd4ebbdb4ab81dbacaf2ddfc62254
Madhusudan Bhukta <166828632+megabytis@users.noreply.github.com>
2025-11-09 21:48:19 +0530

    Implement categories API with admin authorization

---
+++ server/src/routers/categories.js
@@ -0,0 +1,36 @@
+const express = require("express");
+
+const { categoriesModel } = require("../models/category");
+const { userAuth } = require("../middleware/Auth");
+const { userModel } = require("../models/user");
+const { validateNewCategoriesData } = require("../utils/validate");
+
+const categoriesRouter = express.Router();
+
+categoriesRouter.post("/categories", userAuth, async (req, res, next) => {
+  try {
+    const isAdmin = req.user.role === "admin" ? true : false;
+    const { name } = req.body;
+
+    validateNewCategoriesData(req);
+
+    if (!isAdmin) {
+      throw new Error("You are not authorised to Post categories!");
+    } else {
+      const category = new categoriesModel({
+        name: name,
+      });
+
+      const savedCategory = await category.save();
+
+      res.json({
+        messege: "New category Created!",
+        data: savedCategory,
+      });
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = categoriesRouter;
