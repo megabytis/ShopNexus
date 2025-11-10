2a291c15655a785a09ac923f94ac5b4bd88a242f
Madhusudan Bhukta <166828632+megabytis@users.noreply.github.com>
2025-11-10 22:09:49 +0530

    created product schema and model using mongoose Schema

---
+++ server/src/models/product.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    stock: {
+      type: Number,
+      required: true,
+      min: 0,
+      default: 0,
+    },
+    image: {
+      type: String,
+      default: "",
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "category",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const productModel = new mongoose.model("product", productSchema);
+
+module.exports = {
+  productModel,
+};
