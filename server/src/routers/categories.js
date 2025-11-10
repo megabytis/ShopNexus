6a2024617d0c7ee69b61f242400e9bfcd53c702c
Madhusudan Bhukta <166828632+megabytis@users.noreply.github.com>
2025-11-10 12:39:39 +0530

    created more APIs under categories Router
    created APIs like ;
    1. GET /categories
    2. PUT /categories/:id
    3. DELETE /categories/:id

--- server/src/routers/categories.js
+++ server/src/routers/categories.js
@@ -3,7 +3,11 @@ const express = require("express");
 const { categoriesModel } = require("../models/category");
 const { userAuth } = require("../middleware/Auth");
 const { userModel } = require("../models/user");
-const { validateNewCategoriesData } = require("../utils/validate");
+const {
+  validateNewCategoriesData,
+  validateMongoID,
+} = require("../utils/validate");
+const { json } = require("stream/consumers");
 
 const categoriesRouter = express.Router();
 
@@ -28,6 +32,77 @@ categoriesRouter.post("/categories", userAuth, async (req, res, next) => {
         data: savedCategory,
       });
     }
+  } catch (err) {
+    next(err);
+  }
+});
+
+categoriesRouter.get("/categories", userAuth, async (req, res, next) => {
+  try {
+    const categoriesNames = await categoriesModel.find().select("name");
+    res.json({
+      data: categoriesNames,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+categoriesRouter.put("/categories/:id", userAuth, async (req, res, next) => {
+  try {
+    const isAdmin = req.user.role === "admin" ? true : false;
+    const { name } = req.body;
+    const { id } = req.params;
+
+    validateMongoID(id);
+    validateNewCategoriesData(req);
+
+    const categoryFoundOrNot = await categoriesModel.findById(id.toString());
+
+    if (!categoryFoundOrNot) {
+      throw new Error("Invalid MongoID!");
+    }
+
+    if (!isAdmin) {
+      throw new Error("You are not authorised to Post categories!");
+    }
+
+    const foundCategory = await categoriesModel.findByIdAndUpdate(id, {
+      name: name,
+    });
+
+    const newCategory = await foundCategory.save();
+
+    res.json({
+      messege: "Category Modified Successfully! ",
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+categoriesRouter.delete("/categories/:id", userAuth, async (req, res, next) => {
+  try {
+    const isAdmin = req.user.role === "admin" ? true : false;
+    const { id } = req.params;
+
+    if (!isAdmin) {
+      throw new Error("You're not authorised to Delete categories!");
+    }
+
+    const categoryFoundOrNot = await categoriesModel.findById(id.toString());
+
+    if (!categoryFoundOrNot) {
+      throw new Error("Invalid MongoID!");
+    }
+
+    const deleteCategory = await categoriesModel.deleteOne({
+      _id: id.toString(),
+    });
+
+    res.json({
+      messege: "Category Deleted Successfully! ",
+    });
   } catch (err) {
     next(err);
   }
