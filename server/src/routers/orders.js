cca4c854e22b5515653884d9cf76020b827bddae
Madhusudan Bhukta <166828632+megabytis@users.noreply.github.com>
2025-11-15 19:26:05 +0530

    added another API to get orders using order id

--- server/src/routers/orders.js
+++ server/src/routers/orders.js
@@ -5,22 +5,66 @@ const { orderModel } = require("../models/order");
 
 const orderRouter = express.Router();
 
-// Fetching all Orders
+// in /orders/my API, the particular logged in user (be it normal user or admin) can only see his/her personal orders, not of others
 orderRouter.get("/orders/my", userAuth, async (req, res, next) => {
-  const user = req.user;
-  const foundOrders = await orderModel
-    .find({ userId: user._id })
-    .sort({ createdAt: -1 });
+  try {
+    const user = req.user;
+    const foundOrder = await orderModel
+      .find({ userId: user._id })
+      .sort({ createdAt: -1 });
 
-  if (foundOrders.length === 0) {
-    throw new Error("Orders not Found!");
+    if (foundOrder.length === 0) {
+      throw new Error("Orders not Found!");
+    }
+
+    res.json({
+      message: "Orders fetched!",
+      ordersLength: foundOrder.length,
+      orders: foundOrder,
+    });
+  } catch (err) {
+    next(err);
   }
+});
 
-  res.json({
-    message: "Orders fetched!",
-    ordersLength: foundOrders.length,
-    orders: foundOrders,
-  });
+/* /orders/:id behavior:
+
+If the logged-in user is a normal user:
+- They can access only their own order with that ID.
+- If the order belongs to someone else → blocked.
+
+If the logged-in user is an admin:
+- They can access any user's order by ID.
+- No restriction.
+
+*/
+orderRouter.get("/orders/:id", userAuth, async (req, res, next) => {
+  try {
+    const user = req.user;
+    const orderId = req.params.id;
+
+    const foundOrder = await orderModel.findById(orderId);
+
+    if (!foundOrder) {
+      throw new Error("Order not found!");
+    }
+
+    const isAdmin = user.role === "admin";
+    const isOwnerOfTheOrderId =
+      foundOrder.userId.toString() === user._id.toString();
+
+    if (!isOwnerOfTheOrderId && !isAdmin) {
+      throw new Error("Access Denied! You can't view this order!");
+    }
+
+    res.json({
+      messege: "Orders Fetched!",
+      ordersLength: foundOrder.length,
+      orders: foundOrder,
+    });
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = orderRouter;
