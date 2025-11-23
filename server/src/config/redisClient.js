c587016b0223fd63f33150940f287e41dd8fd38c
Madhusudan Bhukta <166828632+megabytis@users.noreply.github.com>
2025-11-23 20:17:26 +0530

    update in redisClient

--- server/src/config/redisClient.js
+++ server/src/config/redisClient.js
@@ -1,10 +1,14 @@
+require("dotenv").config();
+
 const REDIS_HOST = process.env.REDIS_HOST;
-const REDIS_PORT = process.env.REDIS_PORT;
+const REDIS_PORT = parseInt(process.env.REDIS_PORT, 10);
 const REDIS_PASSWORD = process.env.REDIS_PASSWORD;
 const REDIS_USERNAME = process.env.REDIS_USERNAME;
 
 if (!REDIS_HOST || !REDIS_PORT || !REDIS_PASSWORD || !REDIS_USERNAME) {
-  throw new Error("Missing Redis Config: REDIS_HOST, REDIS_PORT, REDIS_PASSWORD, REDIS_USERNAME required")
+  throw new Error(
+    "Missing Redis Config: REDIS_HOST, REDIS_PORT, REDIS_PASSWORD, REDIS_USERNAME required",
+  );
 }
 
 const redisConfig = {
@@ -20,8 +24,10 @@ const redis = new Redis(redisConfig);
 
 // Events
 redis.on("connect", () => console.log("✅ Redis connected"));
-redis.on('ready', () => console.log("⚡ Redis ready for Commands"))
-redis.on("error", (err) => console.error("❌ Redis connection error:", err.message));
+redis.on("ready", () => console.log("⚡ Redis ready for Commands"));
+redis.on("error", (err) =>
+  console.error("❌ Redis connection error:", err.message),
+);
 redis.on("end", () => console.log("🔴 Redis connection closed"));
 
 module.exports = { redis };
