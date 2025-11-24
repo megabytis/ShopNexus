f85de243ad9dce0a7e027b80b746108355751ea5
Madhusudan Bhukta <166828632+megabytis@users.noreply.github.com>
2025-11-24 14:11:41 +0530

    update

--- server/app.js
+++ server/app.js
@@ -17,7 +17,18 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        "http://localhost:5173",
+        "http://localhost:5174",
+        process.env.FRONTEND_URL,
+      ];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   })
