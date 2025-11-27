f245f249e11a59f31e4568cdcfa314209dea26c0
Madhusudan Bhukta <166828632+megabytis@users.noreply.github.com>
2025-11-27 20:54:18 +0530

    rediConfig file

---
+++ server/src/bullmq/redisConfig.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+
+module.exports = {
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
+};
